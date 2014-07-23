7686a43a620a67230718f1703e08d2f3e9046bc6
Radahika <radhika.marvin@berkeley.edu>
2014-07-23 00:13:09 -0700

    added some comments

--- FileCharIterator.java
+++ FileCharIterator.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 /**
  * Returns each character of an input file, one at a time, in binary string
  * format
- * 
+ *
  */
 public class FileCharIterator implements Iterator<String> {
 
@@ -39,7 +39,14 @@ public class FileCharIterator implements Iterator<String> {
         if (this.nextChar == -1) {
             return "";
         } else {
+          //nextChar is literally the 8 bits of a character
+          //Therefore b will contain this 1 byte representation of a character
             Byte b = (byte) this.nextChar;
+            //the b & 0xFF is anding the 8 bits with 1111 1111. By "b & 0xFF" we are forcing b to 
+            //be a positive integer between 0 and 255 bits and because Java performs a widening 
+            //conversion to int, using sign extension and thus the byte gets promoted to an int!
+            //Then we want to format a string of the first 8 places to hold this new Integer and replace
+            //any ' ', with '0'
             String toRtn = String.format("%8s",
                     Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
             try {
